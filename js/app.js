cba586150b9404d384a5edb053927e7e1db7f932
Raphael Faria <raphael@codelogic.me>
2013-11-11 23:26:38 -0200

    Adding matches list (jquery objects)

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@
     playerHtml: '<div class="player"></div>',
     nameHtml: '<span class="name"></span>',
     scoreHtml: '<span class="score"></span>',
+    linesHtml: '<div class="lines"><div class="corner"></div><div class="connector"></div></div>',
+    matchesList: [],
     setup: function(params) {
       var bracket;
 
@@ -17,8 +19,15 @@
       _.forEach(roundsTemp, function(round) {
         var roundElm = $(Bracket.roundHtml);
         roundElm.addClass(numbers[count]);
+
+        if (round.matches.length == 1) {
+          roundElm.append(Bracket.buildGroup(round, 1));
+
+          var m1 = round.matches.shift();
+        };
+
         while(round.matches.length >= 2) {
-          roundElm.append(Bracket.buildGroup(round));
+          roundElm.append(Bracket.buildGroup(round, 2));
 
           var m1 = round.matches.shift();
           var m2 = round.matches.shift();
@@ -27,21 +36,31 @@
         $('.container').append(roundElm);
       });
     },
-    buildGroup: function(round) {
+    buildGroup: function(round, matchesNumber) {
       var groupElm = $(Bracket.groupHtml);
-      groupElm.append(Bracket.buildMatches(round));
+      groupElm.append(Bracket.buildMatches(round, matchesNumber));
+      groupElm.append(Bracket.linesHtml);
       return groupElm;
     },
-    buildMatches: function(round) {
+    buildMatches: function(round, matchesNumber) {
       var matchesElm = $(Bracket.matchesHtml);
-      matchesElm.append(Bracket.buildMatch(round.matches[0]));
-      matchesElm.append(Bracket.buildMatch(round.matches[1]));
+
+      if(matchesNumber != 1) {
+        matchesElm.append(Bracket.buildMatch(round.matches[0]));
+        matchesElm.append(Bracket.buildMatch(round.matches[1]));
+      }
+
+      else if (matchesNumber == 1) {
+        matchesElm.append(Bracket.buildMatch(round.matches[0]));
+      }
+
       return matchesElm;
     },
     buildMatch: function(data) {
       var match = $(Bracket.matchHtml);
       match.append(Bracket.buildPlayer(data.player_1));
       match.append(Bracket.buildPlayer(data.player_2));
+      Bracket.matchesList.push(match);
       return match;
     },
     buildPlayer: function(data) {
@@ -49,7 +68,7 @@
       var nameElm = $(Bracket.nameHtml);
       var scoreElm = $(Bracket.scoreHtml);
       nameElm.text(data.name);
-      scoreElm.text(data.points);
+      scoreElm.text('0');
       playerElm.append(nameElm);
       playerElm.append(scoreElm);
       return playerElm;
