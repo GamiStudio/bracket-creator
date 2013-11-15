2b94a278c78d1c06d09677110771669fe7eaf34c
Raphael Faria <raphaelfmf@gmail.com>
2013-11-15 00:58:27 -0200

    Trying to fix connectors loop

--- js/app.js
+++ js/app.js
@@ -1,35 +1,3 @@
-/*
-
-0
-  0
-1
-    0
-2
-  1
-3
-
-4
-  2
-5
-    1
-6
-  3
-7
-
-
-armazenar ByeTo
-for para achar bye
-  se bye[x] for par
-    byeTo = bye[x]/2
-    joga para p1
-  se bye[x] for impar
-    byeTo = (bye[x] - 1)/2
-    joga para p2
-*/
-
-
-
-
 var myApp = angular.module('brackets-creator', []);
 
 myApp.controller('tournamentOrganizer', function($scope, $http) {
@@ -41,10 +9,30 @@ myApp.controller('tournamentOrganizer', function($scope, $http) {
 
     findByes($scope.rounds);
 
-    console.log($scope.byes);
-
     byeToNextRound($scope.byes, $scope.rounds);
   });
+
+  $scope.getScore = function(result) {
+    var total = [0,0];
+
+    if(result) {
+      for(var i = 0; i < result.length; i++) {
+        if(result[i][0] > result[i][1]) {
+          total[0]++;
+        } else total[1]++;
+      }
+      return total;
+    }
+  }
+
+  $scope.printScore = function(player, result) {
+    total = $scope.getScore(result);
+    if(total) {
+      return total[player];
+    }
+
+    return null;
+  }
 
   $scope.isBye = function(matches, index) {
     if(!matches[index].player_2 || !matches[index].player_1) {
@@ -62,14 +50,12 @@ myApp.controller('tournamentOrganizer', function($scope, $http) {
 
   function byeToNextRound(byes, rounds) {
 
-    // var byeTo;
-
-    // console.log(byes.length);
+    var byeTo;
 
     for(var i = 0; i < (byes.length); i++) {
 
       if(byes[i] % 2 == 0) {
-        var byeTo = byes[i] / 2;
+        byeTo = byes[i] / 2;
         if(rounds[0].matches[byes[i]].player_1) {
           rounds[1].matches[byeTo].player_1 = rounds[0].matches[byes[i]].player_1;
         }
@@ -89,8 +75,22 @@ myApp.controller('tournamentOrganizer', function($scope, $http) {
     }
   }
 
-  $scope.getConnectorsNumber = function(parentIndex) {
-    var number = $scope.rounds[0].matches.length / 2;
-    return new Array(number);
+  $scope.getConnectorsNumber = function(thisRoundLength) {
+    var number = thisRoundLength / 2;
+
+
+
+    console.log(number);
+    var array = [];
+    if(number >= 1) {
+
+      for(var i = 0; i < number; i++) {
+        array.push('a');
+      }
+
+      console.log(array);
+      return array;
+    }
+    else return 0;
   }
 });
